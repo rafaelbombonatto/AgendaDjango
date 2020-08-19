d5275c079864787e331f4c61ef7dcae78bde9b91
rafaelbombonatto <rafael.bombonatto@gmail.com>
2020-08-19 08:56:46 -0300

    Aula de templates

--- core/views.py
+++ core/views.py
@@ -32,3 +32,17 @@ def submit_login(request):
         else:
             messages.error(request,"Usuário ou senha inválidos")
     return redirect('/')
+
+
+def evento(request):
+    return render(request,'evento.html')
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo, data_evento=data_evento, descricao=descricao, usuario=usuario)
+    return redirect ('/')
